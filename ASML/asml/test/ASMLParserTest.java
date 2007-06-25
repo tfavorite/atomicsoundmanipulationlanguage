f56b6a476708aea6564799240f0c577e34f3c3e3
fasmith718 <fasmith718@b567bc54-cb32-0410-8412-5f0927e9a619>
2007-06-25 18:23:54 +0000

    asmlparsertest, testprograms: initial versions of these files.

---
+++ ASML/asml/test/ASMLParserTest.java
@@ -0,0 +1,138 @@
+package asml.test;
+
+import junit.framework.TestCase;
+import asml.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.io.*;
+
+import org.antlr.runtime.*;
+
+public class ASMLParserTest extends TestCase {
+	private ASMLParser mParser;
+	private ASMLLexer  mLexer;
+	
+	public ASMLParserTest(String name) {
+		super(name);		
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		mLexer = new ASMLLexer();
+		mParser = new ASMLParser(new CommonTokenStream(mLexer));
+	}
+	
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		
+		mLexer = null;
+		mParser = null;
+	}
+	
+	public void testSimple(){
+		setLexer(mLexer, "fun int main() end fun");
+		setParser(mParser, mLexer);
+		
+		try {
+			mParser.program();		
+			assertFalse("my assert false message",mParser.hasError);
+		} catch (RecognitionException e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	public void testIfStmt(){	
+		ArrayList<String> testProgs = new ArrayList<String>();
+		try {
+			testProgs = getTestProgs("if tests");
+		} catch (Exception e) {
+			fail("problem loading test progs: "+e.getMessage());
+		}
+		
+		Iterator<String> it = testProgs.iterator();	
+		int i = 1;
+		try {
+			while (it.hasNext()) {
+				setLexer(mLexer, it.next());
+				setParser(mParser, mLexer);
+				mParser.program();
+				assertFalse("failed on program: "+ i, mParser.hasError);
+				i++;
+			}			
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}				
+	}
+	
+	public void testFunDecl(){	
+		ArrayList<String> testProgs = new ArrayList<String>();
+		try {								
+			testProgs = getTestProgs("fun tests");
+		} catch (Exception e) {
+			fail("problem loading test progs: "+e.getMessage());
+		}
+		
+		Iterator<String> it = testProgs.iterator();	
+		int i = 1;
+		try {
+			while (it.hasNext()) {
+				setLexer(mLexer, it.next());
+				setParser(mParser, mLexer);
+				mParser.program();
+				assertFalse("failed on program: "+ i, mParser.hasError);
+				i++;
+			}			
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}	
+	}
+	
+	private void setLexer(Lexer lex, String input){
+		lex.reset();
+		lex.setCharStream(new ANTLRStringStream(input));
+	}
+	
+	private void setParser(Parser par, Lexer lex){
+		par.reset();
+		par.setTokenStream(new CommonTokenStream(lex));
+	}
+	
+	private ArrayList<String> getTestProgs(String tag) 
+			throws Exception{
+		ArrayList<String> list = new ArrayList<String>();
+		BufferedReader in = new BufferedReader(
+				new FileReader("asml\\test\\TestPrograms.txt"));
+		
+		String line, temp = "";
+		while((line = in.readLine())!= null){
+			if((line.compareTo("") != 0) && 
+					(line.charAt(0)=='@') && 
+					(line.substring(1).compareTo(tag) == 0)){
+				temp = "";
+				while((line = in.readLine())!= null){
+					if(line.compareTo("") == 0)
+						continue;
+					if(line.charAt(0)=='#'){ 
+						if(temp.compareTo("")!=0){
+							list.add(temp);
+							temp = "";
+						}//end if it's a program separator
+					}//end if == #
+					else if(line.charAt(0)=='@'){ //it's the next tag, we're done
+						list.add(temp);
+						in.close();
+						return list;
+					}//end if == @
+					else //it is the line of a program
+						temp += (" "+line);
+				}//end while
+			}//end if we are at our tag
+		}//end while
+		
+		if(temp.compareTo("")!=0)
+			list.add(temp);
+		in.close();
+		return list;
+	}
+}
