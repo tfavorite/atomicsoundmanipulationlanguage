31765df64f350a687830a92e7f8a6adeefe04b09
tim.favorite <tim.favorite@b567bc54-cb32-0410-8412-5f0927e9a619>
2007-07-27 03:02:31 +0000

    Added relevant constructors to Wave, also started on implementation of add, and added MixingFloatAudioInputStream (tritonus team code) for use with add.

--- ASML/asml/walker/Wave.java
+++ ASML/asml/walker/Wave.java
@@ -1,9 +1,73 @@
 package asml.walker;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class Wave extends Value {
 
-	public Wave() {
-		// TODO Auto-generated constructor stub
+	protected AudioInputStream mValue;
+	
+	public Wave(AudioInputStream aValue) {
+		mType = Type.WAVE;
+		mValue = aValue;
+		mIsInitialized = true;
+	}
+	
+	public Wave(AudioInputStream aValue, String aName, boolean aIsConst){
+		this(aValue);
+		mName = aName;
+		mIsStorable = true;
+		mIsConst = aIsConst;
+	}
+	
+	public Wave(String file) throws ASMLSemanticException{
+		mType = Type.WAVE;
+		mIsInitialized = true;
+		try {
+			mValue = AudioSystem.getAudioInputStream(new File(file));
+		} catch (UnsupportedAudioFileException e) {
+			throw new ASMLSemanticException("Illegal file type: only WAV files are supported.");
+		} catch (IOException e) {
+			throw new ASMLSemanticException("File does not exist or is otherwise inaccessible at this time.");
+		}
+	}
+	
+	public Wave(String file, String aName, boolean aIsConst) throws ASMLSemanticException{
+		this(file);
+		mName = aName;
+		mIsStorable = true;
+		mIsConst = aIsConst;
+	}
+	
+	/* declaration only */
+	public Wave(String aName, boolean aIsConst){
+		mType = Type.WAVE;
+		mIsConst = aIsConst;
+		mName = aName;
+		mIsInitialized = false;
+	}
+	
+	public Value add(Value rhs) throws ASMLSemanticException{
+		switch(rhs.getType()){
+			case Type.INT:	/* not implemented yet */
+			case Type.FLOAT: /* not implemented yet */
+			case Type.WAVE: 
+				Collection<AudioInputStream> coll = new ArrayList<AudioInputStream>();
+				coll.add(mValue);
+				coll.add(((Wave)rhs).getValue());
+				return new Wave(new MixingFloatAudioInputStream(mValue.getFormat(), coll));
+			default: return super.add(rhs);
+		}
+	}
+	
+	public AudioInputStream getValue(){
+		return mValue;
 	}
 
 }
