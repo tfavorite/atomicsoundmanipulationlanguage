dce2cc087035c2edd4fa9cf75fb3bf14416c2eba
tim.favorite <tim.favorite@b567bc54-cb32-0410-8412-5f0927e9a619>
2007-07-27 22:35:24 +0000

    added a few more operations

--- trunk/ASML/asml/walker/Wave.java
+++ trunk/ASML/asml/walker/Wave.java
@@ -9,6 +9,11 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+import org.tritonus.dsp.ais.AmplitudeAudioInputStream;
+import org.tritonus.share.sampled.FloatSampleBuffer;
+
+import asml.walker.streams.MixingFloatAudioInputStream;
+
 public class Wave extends Value {
 
 	protected AudioInputStream mValue;
@@ -66,6 +71,111 @@ public class Wave extends Value {
 		}
 	}
 	
+	
+	/* (non-Javadoc)
+	 * @see asml.walker.Value#multiply(asml.walker.Value)
+	 */
+	@Override
+	public Value multiply(Value rhs) throws ASMLSemanticException {
+		float scalar;
+		switch (rhs.getType()){
+			case Type.INT: 	
+				scalar = ((Integer)rhs).getValue();
+				break;
+			case Type.FLOAT:
+				scalar = (float)(((Float)rhs).getValue());
+				break;
+			default:
+				return super.multiply(rhs);
+		}
+		AmplitudeAudioInputStream aais = new AmplitudeAudioInputStream(mValue);
+		aais.setAmplitudeLinear(scalar);
+		return new Wave(aais);
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see asml.walker.Value#amplof()
+	 */
+	@Override
+	public Value amplof() throws ASMLSemanticException {
+		AmplitudeAudioInputStream aais = new AmplitudeAudioInputStream(mValue, mValue.getFormat());
+		// TODO 8192 is a totally arbitrary size for the buffer - maybe we'll try different values to see which one gives best performance
+		FloatSampleBuffer buffer = new FloatSampleBuffer(mValue.getFormat().getChannels(), 8192, mValue.getFormat().getSampleRate());
+		aais.read(buffer);
+
+// temporary
+		return null;
+	
+	}
+
+	/* (non-Javadoc)
+	 * @see asml.walker.Value#at(asml.walker.Value, asml.walker.Value)
+	 */
+	@Override
+	public Value at(Value rhs1, Value rhs2) throws ASMLSemanticException {
+		if(rhs1.getType() == Type.TIME && rhs2.getType() == Type.TIME){
+			double start = ((Time)rhs1).getValue();
+			double end = ((Time)rhs2).getValue();
+			float frameSize = mValue.getFormat().getFrameSize();
+			long fstart = (long)(start * frameSize);
+			long flength = (long)((end - start)*frameSize);
+			try {
+				mValue.skip(fstart);
+			} catch (IOException e) {
+				throw new ASMLSemanticException("Cannot read from wave file!");
+			}
+			return new Wave(new AudioInputStream(mValue, mValue.getFormat(), flength));
+		} else if(rhs1.getType() == Type.FREQ && rhs2.getType() == Type.FREQ){}/* not implemented yet */
+		return super.at(rhs1,rhs2);
+	}
+
+	/* (non-Javadoc)
+	 * @see asml.walker.Value#at(asml.walker.Value)
+	 * Fetches one sample in time or frequency domain.
+	 */
+	@Override
+	public Value at(Value rhs) throws ASMLSemanticException {
+		if(rhs.getType() == Type.TIME){
+			double time = ((Time)rhs).getValue();
+			float frameSize = mValue.getFormat().getFrameSize();
+			long fstart = (long)(time * frameSize);
+			try {
+				mValue.skip(fstart);
+			} catch (IOException e) {
+				throw new ASMLSemanticException("Cannot read from wave file!");
+			}
+			return new Wave(new AudioInputStream(mValue, mValue.getFormat(), 1));
+		} else if(rhs.getType() == Type.FREQ){}/* not implemented yet */
+		return super.at(rhs);
+	}
+
+	/* (non-Javadoc)
+	 * @see asml.walker.Value#negate()
+	 */
+	@Override
+	public Value negate() throws ASMLSemanticException {
+		return multiply(new Integer(-1));
+	}
+
+	/* (non-Javadoc)
+	 * @see asml.walker.Value#not()
+	 */
+	@Override
+	public Value not() throws ASMLSemanticException {
+		// TODO Auto-generated method stub
+		return super.not();
+	}
+
+	/* (non-Javadoc)
+	 * @see asml.walker.Value#subtract(asml.walker.Value)
+	 */
+	@Override
+	public Value subtract(Value rhs) throws ASMLSemanticException {
+		// TODO Auto-generated method stub
+		return super.subtract(rhs);
+	}
+
 	public AudioInputStream getValue(){
 		return mValue;
 	}
