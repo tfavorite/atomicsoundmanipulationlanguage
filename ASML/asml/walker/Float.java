7f7fd32b9bd5606b5b6123e733c027ffaa9b448e
tim.favorite <tim.favorite@b567bc54-cb32-0410-8412-5f0927e9a619>
2007-07-27 01:28:00 +0000

    Filled in operators for Amplitude, Float, Frequency, Time, and made minor changes to Integer and Value. Removed assign operator (going to use constructor for this purpose)

--- ASML/asml/walker/Float.java
+++ ASML/asml/walker/Float.java
@@ -14,13 +14,123 @@ public class Float extends Value {
 	public Float(double aValue) {
 		mType = Type.FLOAT;
 		mValue = aValue;
+		mIsInitialized = true;
 	}
 	
-	public Float(double aValue, ASMLString aName, boolean aIsStorable, boolean aIsConst){
+	public Float(double aValue, String aName, boolean aIsStorable, boolean aIsConst){
 		this(aValue);
 		mName = aName;
 		mIsStorable = aIsStorable;
 		mIsConst = aIsConst;
+	}
+	
+	public Float(String aName, boolean aIsConst){
+		mType = Type.FLOAT;
+		mName = aName;
+		mIsConst = aIsConst;
+		mIsInitialized = false;
+	}
+	
+
+	@Override
+	public Value add(Value rhs) throws ASMLSemanticException {
+		switch(rhs.getType()){
+			case Type.INT:	
+				return new Float(mValue + ((Integer)rhs).getValue());
+			case Type.FLOAT:
+				return new Float(mValue + ((Float)rhs).getValue());
+			default:
+				return super.add(rhs);
+				
+		}
+	}
+
+	@Override
+	public Value divide(Value rhs) throws ASMLSemanticException {
+		switch(rhs.getType()){
+			case Type.INT:	
+				return new Float(mValue / ((Integer)rhs).getValue());
+			case Type.FLOAT:
+				return new Float(mValue / ((Float)rhs).getValue());
+			default:
+				return super.add(rhs);
+				
+		}
+	}
+
+	@Override
+	public Value multiply(Value rhs) throws ASMLSemanticException {
+		switch(rhs.getType()){
+			case Type.INT:	
+				return new Float(mValue * ((Integer)rhs).getValue());
+			case Type.FLOAT:
+				return new Float(mValue * ((Float)rhs).getValue());
+			case Type.FREQUENCY:
+				return new Frequency(mValue * ((Frequency)rhs).getValue());
+			case Type.TIME:
+				return new Time(mValue * ((Time)rhs).getValue());
+			case Type.AMPLITUDE:
+				return new Amplitude(mValue * ((Amplitude)rhs).getValue());
+			case Type.WAVE:
+				Wave tW = (Wave) rhs;
+				return tW.multiply(this);
+			default:
+				return super.multiply(rhs);
+			
+		}
+	}
+
+	@Override
+	public Value negate() throws ASMLSemanticException {
+		return new Float(-mValue);
+	}
+
+	@Override
+	public Value relate(Value rhs, String op) throws ASMLSemanticException {
+		double tValue;
+		switch(rhs.getType()){
+			case Type.INT:	
+				tValue = ((Integer)rhs).getValue();
+				break;
+			case Type.FLOAT:
+				tValue = ((Float)rhs).getValue();
+				break;
+			default:
+				return super.relate(rhs,op);
+		}
+
+		if (op.equals("<"))
+			if (mValue < tValue) return new Integer(1);
+			else return new Integer(0);
+		else if (op.equals("<="))
+			if (mValue <= tValue) return new Integer(1);
+			else return new Integer(0);
+		else if (op.equals(">"))
+			if (mValue > tValue) return new Integer(1);
+			else return new Integer(0);
+		else if (op.equals(">="))
+			if (mValue >= tValue) return new Integer(1);
+			else return new Integer(0);
+		else if (op.equals("=="))
+			if (mValue == tValue) return new Integer(1);
+			else return new Integer(0);
+		else if (op.equals("!="))
+			if (mValue != tValue) return new Integer(1);
+			else return new Integer(0);
+		return super.relate(rhs, op);
+	}
+
+	@Override
+	public Value subtract(Value rhs) throws ASMLSemanticException {
+		switch(rhs.getType()){
+			case Type.INT:	
+				return new Float(mValue - ((Integer)rhs).getValue());
+			case Type.FLOAT:
+				return new Float(mValue - ((Float)rhs).getValue());
+			default:
+				return super.subtract(rhs);
+			
+		}
 	}
 
 	public double getValue() {
--- ASML/asml/walker/Frequency.java
+++ ASML/asml/walker/Frequency.java
@@ -13,13 +13,94 @@ public class Frequency extends Value {
 	public Frequency(double aValue) {
 		mType = Type.FREQ;
 		mValue = aValue;
+		mIsInitialized = true;
 	}
 	
-	public Frequency(double aValue, ASMLString aName, boolean aIsStorable, boolean aIsConst){
+	public Frequency(double aValue, String aName, boolean aIsStorable, boolean aIsConst){
 		this(aValue);
 		mName = aName;
 		mIsStorable = aIsStorable;
 		mIsConst = aIsConst;
+	}
+	
+	public Frequency(String aName, boolean aIsConst){
+		mType = Type.FREQ;
+		mName = aName;
+		mIsConst = aIsConst;
+		mIsInitialized = false;
+	}
+	
+
+	@Override
+	public Value add(Value rhs) throws ASMLSemanticException {
+		if(rhs.getType() == Type.FREQ)
+			return new Frequency(mValue + ((Frequency)rhs).getValue());
+		
+		return super.add(rhs);
+	}
+
+	@Override
+	public Value divide(Value rhs) throws ASMLSemanticException {
+		switch(rhs.getType()){
+			case Type.INT:	
+				return new Frequency(mValue / ((Integer)rhs).getValue());
+			case Type.FLOAT:
+				return new Frequency(mValue / ((Float)rhs).getValue());
+			case Type.FREQ:
+				return new Float(mValue / ((Frequency)rhs).getValue());
+			default:
+				return super.divide(rhs);
+				
+		}
+	}
+
+	@Override
+	public Value multiply(Value rhs) throws ASMLSemanticException {
+		switch(rhs.getType()){
+			case Type.INT:	
+				return new Frequency(mValue * ((Integer)rhs).getValue());
+			case Type.FLOAT:
+				return new Frequency(mValue * ((Float)rhs).getValue());
+			default:
+				return super.multiply(rhs);
+			
+		}
+	}
+
+	@Override
+	public Value relate(Value rhs, String op) throws ASMLSemanticException {
+		double tValue;
+		if(rhs.getType() != Type.FREQ)
+			return super.relate(rhs,op);
+
+		tValue = ((Frequency)rhs).getValue();
+		if (op.equals("<"))
+			if (mValue < tValue) return new Integer(1);
+			else return new Integer(0);
+		else if (op.equals("<="))
+			if (mValue <= tValue) return new Integer(1);
+			else return new Integer(0);
+		else if (op.equals(">"))
+			if (mValue > tValue) return new Integer(1);
+			else return new Integer(0);
+		else if (op.equals(">="))
+			if (mValue >= tValue) return new Integer(1);
+			else return new Integer(0);
+		else if (op.equals("=="))
+			if (mValue == tValue) return new Integer(1);
+			else return new Integer(0);
+		else if (op.equals("!="))
+			if (mValue != tValue) return new Integer(1);
+			else return new Integer(0);
+		return super.relate(rhs, op);
+	}
+
+	@Override
+	public Value subtract(Value rhs) throws ASMLSemanticException {
+		if(rhs.getType() != Type.FREQ)
+			return super.subtract(rhs);
+			
+		return new Frequency(mValue - ((Frequency)rhs).getValue());
 	}
 
 	public double getValue() {
