c391d26c97677f2584295290ff0e2b28d3276290
tim.favorite <tim.favorite@b567bc54-cb32-0410-8412-5f0927e9a619>
2007-07-30 03:14:59 +0000

    Added frequency range functionality

--- trunk/ASML/asml/walker/ASMLWave.java
+++ trunk/ASML/asml/walker/ASMLWave.java
@@ -12,6 +12,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import org.tritonus.dsp.ais.AmplitudeAudioInputStream;
 import org.tritonus.share.sampled.FloatSampleBuffer;
 
+import asml.walker.streams.ConvolvingFloatAudioInputStream;
 import asml.walker.streams.MixingFloatAudioInputStream;
 
 public class ASMLWave extends Value {
@@ -155,7 +156,11 @@ public class ASMLWave extends Value {
 				throw new ASMLSemanticException("Cannot read from wave file!");
 			}
 			return new ASMLWave(new AudioInputStream(mValue, mValue.getFormat(), flength));
-		} else if(rhs1.getType() == Type.FREQ && rhs2.getType() == Type.FREQ){}/* not implemented yet */
+		} else if(rhs1.getType() == Type.FREQ && rhs2.getType() == Type.FREQ){
+			/* Windowed-sinc implementation */
+			float[] filter = windowedSinc(((ASMLFrequency)rhs1).getValue(), ((ASMLFrequency)rhs2).getValue());
+			return new ASMLWave(new ConvolvingFloatAudioInputStream(mValue.getFormat(), mValue, filter));
+		}
 		return super.at(rhs1,rhs2);
 	}
 
@@ -175,7 +180,12 @@ public class ASMLWave extends Value {
 				throw new ASMLSemanticException("Cannot read from wave file!");
 			}
 			return new ASMLWave(new AudioInputStream(mValue, mValue.getFormat(), 1));
-		} else if(rhs.getType() == Type.FREQ){}/* not implemented yet */
+		} else if(rhs.getType() == Type.FREQ){
+			// This too, is a range, but a range of 1 - rhs to rhs + 1...
+			double freq = ((ASMLFrequency)rhs).getValue();
+			float[] filter = windowedSinc(freq, freq + 1.0);
+			return new ASMLWave(new ConvolvingFloatAudioInputStream(mValue.getFormat(), mValue, filter));
+		}
 		return super.at(rhs);
 	}
 
@@ -207,6 +217,61 @@ public class ASMLWave extends Value {
 
 	public AudioInputStream getValue(){
 		return mValue;
+	}
+	
+	private float[] windowedSinc(double start, double end){
+		double sum;
+		int windowSize = 800;
+		if (windowSize % 2 == 1) windowSize += 1;
+		
+		double lower[] = new double[windowSize + 1];
+		double upper[] = new double[windowSize + 1];
+		double filter[] = new double[windowSize + 1];
+		double fstart = start/mValue.getFormat().getSampleRate();
+		double fend = end/mValue.getFormat().getSampleRate();
+		
+		//calculate lower - it's a low pass filter with cutoff of start
+		for (int i=0; i<=windowSize; i++){
+			if(i == windowSize/2) lower[i] = 2 * Math.PI * fstart;
+			else lower[i] = Math.sin(2*Math.PI*fstart*(i-windowSize/2))/(i-windowSize/2);
+			
+			lower[i] = lower[i] * (0.42-0.5*Math.cos(2*Math.PI*i/windowSize)+0.08*Math.cos(4*Math.PI*i/windowSize));	
+		}
+		
+		// normalize lower
+		sum = 0.0;
+		for (int i=0; i<=windowSize; i++) sum +=lower[i];
+		for (int i=0; i<=windowSize; i++) lower[i] = lower[i]/sum;
+		
+		// same thing for upper now, cutoff is end instead of start
+		for (int i=0; i<=windowSize; i++){
+			if(i == windowSize/2) upper[i] = 2 * Math.PI * fend;
+			else upper[i] = Math.sin(2*Math.PI*fend*(i-windowSize/2))/(i-windowSize/2);
+			
+			upper[i] = upper[i] * (0.42-0.5*Math.cos(2*Math.PI*i/windowSize)+0.08*Math.cos(4*Math.PI*i/windowSize));	
+		}
+		
+		// normalize upper
+		sum = 0.0;
+		for (int i=0; i<=windowSize; i++) sum +=upper[i];
+		for (int i=0; i<=windowSize; i++) upper[i] = upper[i]/sum;		
+		
+		// now change upper into a high-pass filter using spectral inversion
+		for (int i=0; i<=windowSize; i++) upper[i] = -upper[i];
+		upper[windowSize/2] += 1.0;
+		
+		// now add lower and upper to make a band-reject filter
+		for (int i=0; i<=windowSize; i++) filter[i] = lower[i] + upper[i];
+		
+		//do spectral inversion on band-reject filter to make our band-pass filter!
+		for (int i=0; i<=windowSize; i++) filter[i] = -filter[i];
+		filter[windowSize/2] += 1.0;
+			
+		// we're done! now use this with ConvolvingInputStream.
+		
+		float[] floatfilter = new float[windowSize + 1];
+		for (int i=0; i<=windowSize; i++) floatfilter[i] = (float)filter[i];
+		return floatfilter;
 	}
 
 }
