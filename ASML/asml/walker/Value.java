7771a50df3d497cb10629b652509998443b160dd
tim.favorite <tim.favorite@b567bc54-cb32-0410-8412-5f0927e9a619>
2007-08-01 21:52:01 +0000

    documentin'

--- ASML/asml/walker/Value.java
+++ ASML/asml/walker/Value.java
@@ -12,39 +12,102 @@ package asml.walker;
  *
  */
 public abstract class Value {
+	/** The Type of this Value (see Type class) */
 	protected int mType;
+	/** The name of this Value object. */
 	protected String mName        = null;
+	/** Represents whether this Value object is storable or not (is it a variable?) */
 	protected boolean mIsStorable = false;
+	/** Represents whether this Value object is a constant variable */
 	protected boolean mIsConst    = false;
+	/** Represents whether this Value object has been initialized */
 	protected boolean mIsInitialized = false;
 	
 	//public accessors
+	/**
+	 * Gets the type.
+	 * @return the type (see Type class)
+	 */
 	public int getType(){return mType;}
+	/**
+	 * Is this object storable?
+	 * @return whether it's storable
+	 */
 	public boolean isStorable(){return mIsStorable;}
+	/**
+	 * Is this object a constant variable?
+	 * @return whether it's a constant
+	 */
 	public boolean isConst(){return mIsConst;}
+	/**
+	 * Gets object's name.
+	 * @return the name of this object
+	 */
 	public String getName() {return mName;}
+	/**
+	 * Has this object been initialized?
+	 * @return if it's been initialized
+	 */
 	public boolean isInitialized() {return mIsInitialized;}
 	
+	/**
+	 * Performs an add operation
+	 * @param rhs The right hand side of the operation
+	 * @return the value of the add operation
+	 * @throws ASMLSemanticException if operand types were incompatible with this operation
+	 */
 	public Value add(Value rhs) throws ASMLSemanticException{
 		throw new ASMLSemanticException("Illegal Operation: add");
 	}
 	
+	/**
+	 * Performs a subtraction operation
+	 * @param rhs the right hand side of the operation
+	 * @return the value of the subtraction operation
+	 * @throws ASMLSemanticException if operand types were incompatible with this operation
+	 */
 	public Value subtract(Value rhs) throws ASMLSemanticException{
 		throw new ASMLSemanticException("Illegal Operation: subtract");
 	}
 	
+	/**
+	 * Performs a multiplication operation
+	 * @param rhs the right hand side of the operation
+	 * @return the value of the multiplication operation
+	 * @throws ASMLSemanticException if operand types were incompatible with this operation
+	 */
 	public Value multiply(Value rhs) throws ASMLSemanticException{
 		throw new ASMLSemanticException("Illegal Operation: multiply");
 	}
-	
+
+	/**
+	 * Performs a division operation
+	 * @param rhs the right hand side of the operation
+	 * @return the value of the division operation
+	 * @throws ASMLSemanticException if operand types were incompatible with this operation
+	 */
 	public Value divide(Value rhs) throws ASMLSemanticException{
 		throw new ASMLSemanticException("Illegal Operation: divide");
 	}
 	
+	/**
+	 * Performs a modulus operation
+	 * @param rhs the right hand side of the operation
+	 * @return the value of the modulus operation
+	 * @throws ASMLSemanticException if operand types were incompatible with this operation
+	 */
 	public Value mod(Value rhs) throws ASMLSemanticException{
 		throw new ASMLSemanticException("Illegal Operation: mod");
 	}
 	
+	/**
+	 * Performs a relational expression
+	 * @param rhs the right hand side of the operation
+	 * @param op the operator (<, <=, >, >=, ==, !=)
+	 * @return the result of the relational operation - an ASMLInteger with the value 0
+	 * (false) or 1 (true)
+	 * @throws ASMLSemanticException if operand types were incompatible with this operation
+	 */
 	public Value relate(Value rhs, String op) throws ASMLSemanticException{
 		throw new ASMLSemanticException("Illegal Operation: '" + op + "'");
 	}
@@ -65,18 +128,40 @@ public abstract class Value {
 		throw new ASMLSemanticException("Illegal Operation: '!'");
 	}
 	
+	/**
+	 * Negates the value of this object
+	 * @return the negative of this object's value
+	 * @throws ASMLSemanticException if object could not be negated
+	 */
 	public Value negate() throws ASMLSemanticException{
 		throw new ASMLSemanticException("Illegal Operation: 'negative'");
 	}
 	
+	/**
+	 * Gets the amplitude of this object's value (note: this value must be a wave)
+	 * @return the amplitude of this wave
+	 * @throws ASMLSemanticException if the object was not a wave
+	 */
 	public Value amplof() throws ASMLSemanticException{
 		throw new ASMLSemanticException("Illegal Operation: amplof");
 	}
 	
+	/**
+	 * Gets a sample of this object (must be wave) at the time or frequency specified
+	 * @param rhs the time or frequency being specified
+	 * @return the sample of the wave
+	 * @throws ASMLSemanticException if the object was not a wave
+	 */
 	public Value at(Value rhs) throws ASMLSemanticException{
 		throw new ASMLSemanticException("Illegal Operation: at");
 	}
-	
+
+	/**
+	 * Gets a sample range of this object (must be wave) at the times or frequencies specified
+	 * @param rhs the time or frequency range being specified
+	 * @return the sample range of the wave
+	 * @throws ASMLSemanticException if the object was not a wave
+	 */
 	public Value at(Value rhs1, Value rhs2) throws ASMLSemanticException{
 		throw new ASMLSemanticException("Illegal Operation: at");
 	}
