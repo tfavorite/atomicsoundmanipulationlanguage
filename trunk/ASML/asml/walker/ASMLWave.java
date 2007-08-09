5c535e9f87aba6b27495875b871c865930a034b1
tim.favorite <tim.favorite@b567bc54-cb32-0410-8412-5f0927e9a619>
2007-08-09 13:17:35 +0000

    reverting back to v.103

--- trunk/ASML/asml/walker/ASMLWave.java
+++ trunk/ASML/asml/walker/ASMLWave.java
@@ -208,14 +208,11 @@ public class ASMLWave extends Value {
 			} catch (IOException e) {
 				throw new ASMLSemanticException("Cannot read from wave file!");
 			}
-			return new ASMLWave(new AudioInputStream(mValue, mValue.getFormat(), flength), mName);
+			return new ASMLWave(new AudioInputStream(mValue, mValue.getFormat(), flength));
 		} else if(rhs1.getType() == Type.FREQ && rhs2.getType() == Type.FREQ){
 			/* Windowed-sinc implementation */
 			float[] filter = windowedSinc(((ASMLFrequency)rhs1).getValue(), ((ASMLFrequency)rhs2).getValue());
-			mValue = new ConvolvingFloatAudioInputStream(mValue.getFormat(), mValue, filter);
-			write(rhs1.toString() + rhs2.toString() +".wav");
-			return new ASMLWave(mName, rhs1.toString() + rhs2.toString() +".wav");
-		//	return new ASMLWave(new ConvolvingFloatAudioInputStream(mValue.getFormat(), mValue, filter), mName);
+			return new ASMLWave(new ConvolvingFloatAudioInputStream(mValue.getFormat(), mValue, filter));
 		}
 		return super.at(rhs1,rhs2);
 	}
